a334299a5fb44bd9d1a9e13c5fc3b19f5ac82fe3
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-25 22:29:36 +0900

    2_add_two_numbers 두 번째 풀이

--- leetcode/medium/2_add_two_numbers.py
+++ leetcode/medium/2_add_two_numbers.py
@@ -26,3 +26,36 @@ class Solution:
         if node.next is None:
             return result
         return result + self.makeNumber(node.next, ten*10)
+    
+
+# 두 번째 풀이
+    # Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head_node = ListNode()
+        tail_node = head_node
+
+        cur_node1 = l1
+        cur_node2 = l2
+
+        while True:
+            tail_node.val += cur_node1.val + cur_node2.val
+            if tail_node.val >= 10:
+                tail_node.val -= 10
+                tail_node.next = ListNode(1)
+
+            if cur_node1.next is None and cur_node2.next is None:
+                return head_node
+            
+            if tail_node.next is None:
+                tail_node.next = ListNode()
+                
+            tail_node = tail_node.next
+
+            cur_node1 = ListNode() if cur_node1.next is None else cur_node1.next
+            cur_node2 = ListNode() if cur_node2.next is None else cur_node2.next
