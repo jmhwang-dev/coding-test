3665e07d3ae78dad6405960af4d439a85e21796b
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-03-25 16:46:11 +0900

    hash table

--- leetcode/easy/1_two_sum.py
+++ leetcode/easy/1_two_sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # brute force
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -5,4 +7,31 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+        return []
 
+# hash table: Two-pass
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+            
+        return []
+    
+# hash table: One-pass
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []
