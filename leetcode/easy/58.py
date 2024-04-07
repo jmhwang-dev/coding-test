0fb02648b7daf076821c16a4ff0c4fddf5d2576a
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-07 23:40:44 +0900

    string - 58. Length of Last Word

---
+++ leetcode/easy/58.py
@@ -0,0 +1,8 @@
+# 58. Length of Last Word
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.strip().split(' ')[-1])
