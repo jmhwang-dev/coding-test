a69f4a29d1e5f2adbfeb2a45f25b3b71e26d2fc0
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-07 23:14:58 +0900

    35. Search Insert Position: 두 번째 풀이

--- leetcode/easy/35.py
+++ leetcode/easy/35.py
@@ -1,4 +1,28 @@
 # 35. Search Insert Position
+
+# 2
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            middle = (end + start) // 2
+            value = nums[middle]
+
+            if target == value:
+                return middle
+            elif target < value:
+                end = middle - 1
+            elif target > value:
+                start = middle + 1
+        return start
+
+# 1
 class Solution(object):
     def searchInsert(self, nums, target):
         """
