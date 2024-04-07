f8c4f6e7569d1d6d731d0227a522369f1354b7d5
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-07 23:42:45 +0900

    58. Length of Last Word: 두 번째 풀이

--- leetcode/easy/58.py
+++ leetcode/easy/58.py
@@ -1,4 +1,14 @@
 # 58. Length of Last Word
+# 2
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split()[-1])
+    
+# 1
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
