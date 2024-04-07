0b8e4004d32969cfdc02511436b7656f9299bcdc
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-07 22:18:44 +0900

    28. Find the Index of the First Occurrence in a String

--- leetcode/easy/28.py
+++ leetcode/easy/28.py
@@ -1,3 +1,19 @@
+# 2
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        index = 0
+        while index < len(haystack):
+            if haystack[index:index+len(needle)] == needle:
+                return index
+            index += 1
+        return -1
+
+# 1
 class Solution(object):
     def strStr(self, haystack, needle):
         """
