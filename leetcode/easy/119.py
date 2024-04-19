d7361793c33cc82d9203b3ee2dc7a308d2ebf90e
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-19 11:19:10 +0900

    DP 2nd: 119. Pascal's Triangle II

--- leetcode/easy/119.py
+++ leetcode/easy/119.py
@@ -1,3 +1,13 @@
+# 2: append 제외
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for _ in range(rowIndex):
+            row = [1] + [row[i] + row[i+1] for i in range(len(row)-1)] + [1]
+        return row
+
+
+# 1
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         ans = [[1]] # 0
