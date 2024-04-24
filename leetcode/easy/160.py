dc967197a3a6fd25171039d5ffc1a7e66bf2476c
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-04-24 21:34:18 +0900

    two pointer: 160. Intersection of Two Linked Lists

--- leetcode/easy/160.py
+++ leetcode/easy/160.py
@@ -4,6 +4,31 @@
 #         self.val = x
 #         self.next = None
 
+# 2: two pointer
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        nodeA = headA
+        nodeB = headB
+
+        while nodeA or nodeB:
+            if not nodeA:
+                nodeA = headB
+            if not nodeB:
+                nodeB = headA
+            if nodeA == nodeB:
+                return nodeA
+            
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+        return 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# 1: hash table 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         hash_table = {}
