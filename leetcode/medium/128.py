09ecefbc73762d5f5441b0ef4a6d9ed94a776faa
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-03-17 17:28:30 +0900

    hash map: 128. Longest Consecutive Sequence

--- leetcode/medium/128.py
+++ leetcode/medium/128.py
@@ -4,22 +4,19 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
-        sorted_nums = sorted(nums)
-        total_len = len(nums)
         ans = 1
-        output = [1]
-        for i in range(total_len):
-            if i + 1 >= total_len:
-                output.append(ans)
-                break
-            if sorted_nums[i+1] == sorted_nums[i]:
-                continue
-            if sorted_nums[i+1] - sorted_nums[i] == 1:
-                ans += 1
-            else:
-                output.append(ans)
-                ans = 1
-        return max(output)
+        unique_nums = set(nums)
+
+        for num in unique_nums:
+            if num - 1 not in unique_nums:
+                cur_num = num
+                cur_len = 1
+                while cur_num + 1 in unique_nums:
+                    cur_num += 1
+                    cur_len += 1
+                    ans = max(ans, cur_len)
+        return ans
+            
 
         
 sol = Solution()
