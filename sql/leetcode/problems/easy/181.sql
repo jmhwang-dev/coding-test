10197ca98ec39343bb6d77c75acb2e90cc92270a
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-08-29 00:10:29 +0900

    sub query

--- sql/leetcode/problems/easy/181.sql
+++ sql/leetcode/problems/easy/181.sql
@@ -1,7 +1,12 @@
--- ans1
+-- ans1 - best
 select e.name as Employee
 from Employee e
 left join Employee m
 on e.managerId = m.id
 where e.salary > m.salary
 
+-- ans2
+
+select e.name as Employee
+from Employee e
+where e.salary > (select m.salary from Employee m where m.id = e.managerId)
