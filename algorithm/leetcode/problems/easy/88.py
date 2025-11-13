cca087e09435c7bb4f14f771991a5347c68c3310
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-11-14 00:02:32 +0900

    easy(algorithm): 88.py - two pointer...

--- algorithm/leetcode/problems/easy/88.py
+++ algorithm/leetcode/problems/easy/88.py
@@ -1,26 +1,79 @@
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         if n == 0:
+#             return
+        
+#         n_start = 0
+#         m_start = 0
+#         m_end = m
+#         while True:
+#             if n_start == n:
+#                 return
+            
+#             if m_start == m_end:
+#                 nums1[m_start:] = nums2[n_start:]
+#                 return
+
+#             if nums1[m_start] > nums2[n_start]:
+#                 nums1[m_start+1:m_end+1] = nums1[m_start:m_end]
+#                 nums1[m_start] = nums2[n_start]
+#                 n_start += 1
+#                 m_end += 1
+            
+#             m_start += 1
+
+
+# ans2: 2025-11-13
+from typing import List
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+
+        if m == 0:
+            nums1[:] = nums2[:]
+            return
         if n == 0:
             return
-        
-        n_start = 0
-        m_start = 0
-        m_end = m
-        while True:
-            if n_start == n:
-                return
-            
-            if m_start == m_end:
-                nums1[m_start:] = nums2[n_start:]
-                return
 
-            if nums1[m_start] > nums2[n_start]:
-                nums1[m_start+1:m_end+1] = nums1[m_start:m_end]
-                nums1[m_start] = nums2[n_start]
-                n_start += 1
-                m_end += 1
+        while True:
+            print("nums1: ", nums1, "   nums2:", nums2, "   m:", m, "   n:", n)
+
+            if m == 0:
+                nums1[:m+n] = nums2[:n]
+                return
+            elif n == 0:
+                return
             
-            m_start += 1
+            if nums1[m-1] <= nums2[n-1]:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+
+
+# nums1 = [1,2,3,0,0,0]
+# m = 3
+# nums2 = [2,5,6]
+# n = 3
+
+# nums1 = [2,0]
+# m = 1
+# nums2 = [1]
+# n = 1
+
+nums1 = [4,0,0,0,0,0]
+m = 1
+nums2 = [1,2,3,5,6]
+n = 5
+
+
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
+print(nums1)
