22deeaf3d08ce6d2864269def7dbe61e310a2438
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-11-15 21:20:20 +0900

    medium(sql): add best ans for 608: O(n) left join and group by

--- sql/leetcode/problems/medium/608.sql
+++ sql/leetcode/problems/medium/608.sql
@@ -9,3 +9,16 @@ case
     when id in (select id from ParentNode) then 'Inner' else 'Leaf'
 end as type
 from Tree
+
+-- best
+SELECT 
+    t.id,
+    CASE 
+        WHEN t.p_id IS NULL THEN 'Root'
+        WHEN COUNT(c.id) = 0 THEN 'Leaf'
+        ELSE 'Inner'
+    END AS type
+FROM Tree t
+LEFT JOIN Tree c ON t.id = c.p_id
+GROUP BY t.id, t.p_id
+ORDER BY t.id;
