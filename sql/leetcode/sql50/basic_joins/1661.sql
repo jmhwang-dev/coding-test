58a529e6689cd4041d1b3e51a7d626aa8d03cbcb
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-19 23:57:52 +0900

    solve again: 1661

--- sql/leetcode/sql50/basic_joins/1661.sql
+++ sql/leetcode/sql50/basic_joins/1661.sql
@@ -22,10 +22,53 @@ FROM (
 GROUP BY 
     machine_id;
 
--- ans2*
+-- ans2
 select s.machine_id, round(avg(e.timestamp - s.timestamp)::numeric, 3) as processing_time
 from Activity s
 join Activity e
 on s.machine_id = e.machine_id and s.process_id = e.process_id
 where s.activity_type = 'start' and e.activity_type = 'end'
 group by s.machine_id;
+
+-- ans3
+with StartTimestamp as (
+    select *
+    from Activity
+    where activity_type = 'start'
+),
+
+EndTimestamp as (
+    select *
+    from Activity
+    where activity_type = 'end'
+),
+
+ProcessingTime as (
+    select
+        e.machine_id,
+        e.timestamp - s.timestamp as processing_time
+    from EndTimestamp e
+    left join StartTimestamp s
+    on e.machine_id = s.machine_id and e.process_id = s.process_id
+)
+
+select machine_id, round(avg(processing_time)::numeric, 3) as processing_time
+from ProcessingTime
+group by machine_id
+
+
+-- best
+SELECT 
+    a1.machine_id, 
+    ROUND(AVG(a2.timestamp - a1.timestamp)::numeric, 3) AS processing_time
+FROM 
+    Activity a1
+JOIN 
+    Activity a2 
+ON 
+    a1.machine_id = a2.machine_id 
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start' 
+    AND a2.activity_type = 'end'
+GROUP BY 
+    a1.machine_id;
