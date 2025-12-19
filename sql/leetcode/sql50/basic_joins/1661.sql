a4abca0d45d5de6c72987fd0ce07b6c560296ea9
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-20 00:04:49 +0900

    solve again: 1661

--- sql/leetcode/sql50/basic_joins/1661.sql
+++ sql/leetcode/sql50/basic_joins/1661.sql
@@ -57,7 +57,23 @@ from ProcessingTime
 group by machine_id
 
 
--- best
+-- best1 가독성 + 정석 (Self-Join)
+SELECT 
+    a1.machine_id, 
+    ROUND(AVG(a2.timestamp - a1.timestamp)::numeric, 3) AS processing_time
+FROM 
+    Activity a1
+JOIN 
+    Activity a2 
+ON 
+    a1.machine_id = a2.machine_id 
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start' 
+    AND a2.activity_type = 'end'
+GROUP BY 
+    a1.machine_id;
+
+-- best2 성능 중심
 SELECT 
     a1.machine_id, 
     ROUND(AVG(a2.timestamp - a1.timestamp)::numeric, 3) AS processing_time
