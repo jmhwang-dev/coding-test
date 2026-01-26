dbc358977a2a70a31f4f535060f75810519d9b11
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2026-01-26 14:13:41 +0900

    update 79

--- algorithm/leetcode/problems/dfs/medium/79.py
+++ algorithm/leetcode/problems/dfs/medium/79.py
@@ -1,31 +1,25 @@
 from typing import List
 
+# - back-tracking
+# - dfs
+
 class Solution:
-    """
-    - back-tracking
-    - dfs
-    """
     def exist(self, board: List[List[str]], word: str) -> bool:
 
         word_len = len(word)
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
         def dfs(i, j, word_index):
 
             if word_index == word_len:
                 return True
 
-            if i == len(board) or i < 0:
-                return False
-
-            if j == len(board[0]) or j < 0:
-                return False
-
-            if board[i][j] == '#':
+            if i == len(board) or i < 0 or j == len(board[0]) or j < 0:
                 return False
             
             if board[i][j] != word[word_index]:
                 return False
 
-            directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
             origin_char = board[i][j]
             board[i][j] = '#'
 
@@ -38,6 +32,6 @@ class Solution:
 
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if dfs(r,c,0):
+                if board[r][c] == word[0] and dfs(r,c,0):
                     return True
         return False
