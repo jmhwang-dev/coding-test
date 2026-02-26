b327d6d42cd7c7109f881a412e92df1c744e357b
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2026-02-26 19:04:23 +0900

    bfs: sol2 for 994

--- algorithm/leetcode/problems/bfs/medium/994.py
+++ algorithm/leetcode/problems/bfs/medium/994.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+# ans1: append [row, col, time] 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
@@ -34,3 +35,42 @@ class Solution:
                 rotten_oranges.append([adj_row, adj_col, time+1])
 
         return time if fresh_orange_count == 0 else -1
+
+
+# ans2: 사이즈 측정 방식
+
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        width, height = len(grid[0]), len(grid)
+
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+
+        rotten_oranges = deque()
+        fresh_orange_count = 0
+        for curr_row in range(height):
+            for curr_col in range(width):
+                if grid[curr_row][curr_col] == 2:
+                    rotten_oranges.append([curr_row, curr_col])
+                elif grid[curr_row][curr_col] == 1:
+                    fresh_orange_count += 1
+
+        result = 0
+    
+        while rotten_oranges and fresh_orange_count > 0:
+            for _ in range(len(rotten_oranges)):
+                row, col = rotten_oranges.popleft()
+
+                for dx, dy in directions:
+                    adj_row, adj_col = row + dx, col + dy
+                    
+                    if not (0 <= adj_row < height) or not (0 <= adj_col < width) or grid[adj_row][adj_col] != 1:
+                        continue
+                    
+                    fresh_orange_count -= 1
+                    grid[adj_row][adj_col] = 2
+                    rotten_oranges.append([adj_row, adj_col])
+            result += 1
+        return result if fresh_orange_count == 0 else -1
