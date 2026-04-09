374af25186f2ad9f24f7f7acf07965e066c17631
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2026-04-09 23:03:12 +0900

    boj: backtracking - 15652

---
+++ algorithm/baekjoon/dfs/backtracking/15652/sol.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+N, M = list(map(int, input().split()))
+
+def dfs(num):
+
+    if len(result) == M:
+        print(*result)
+        return
+
+    for i in range(num, N+1):
+        result.append(i)
+        dfs(i)
+        result.pop()
+
+result = []
+dfs(1)
